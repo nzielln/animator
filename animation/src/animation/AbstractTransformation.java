faddbc6a5c12463eb70811c551aa307a375d1a49
Ellah Nzikoba <ellahnzikoba@gmail.com>
2021-04-05 17:18:14 -0400

    post-oh changes

--- animation/src/animation/AbstractTransformation.java
+++ animation/src/animation/AbstractTransformation.java
@@ -55,60 +55,6 @@ public abstract class AbstractTransformation implements Transformation {
     return null;
   }
   
-  //OTHER------------------------------------------------------------------------------------------
-  @Override
-  public Shape moveShape(Shape shape, float toX, float toY, int timeStart, int timeEnd) {
-    if (timeStart < 0 || timeEnd < 0) {
-      throw new IllegalArgumentException("Start and end time must be positive");
-    }
-  
-    if (shape.getType().equals("OVAL")) {
-      Shape s = new Oval(shape.getName(), toX,
-              toY, shape.getX(), shape.getY(),
-              shape.getColor().getR(), shape.getColor().getG(), shape.getColor().getB());
-      s.setAppears(timeStart);
-      s.setDisappears(timeEnd);
-      return s;
-    } else if (shape.getType().equals("RECTANGLE")) {
-      Shape s = new Rectangle(shape.getName(), toX,
-              toY, shape.getX(), shape.getY(),
-              shape.getColor().getR(), shape.getColor().getG(), shape.getColor().getB());
-      s.setAppears(timeStart);
-      s.setDisappears(timeEnd);
-      return s;
-    }
-  
-    return null;
-  }
-  
-  @Override
-  public Shape scaleShape(Shape shape, int toX, int toY, int timeStart, int timeEnd) {
-    if (timeStart < 0 || timeEnd < 0) {
-      throw new IllegalArgumentException("Start and end time must be positive");
-    }
-    if (shape.getType().equals("OVAL")) {
-      Shape s = new Oval(shape.getName(), shape.getPositionX(),
-              shape.getPositionY(), toX, toY,
-              shape.getColor().getR(), shape.getColor().getG(), shape.getColor().getB());
-      s.setAppears(timeStart);
-      s.setDisappears(timeEnd);
-      return s;
-    } else if (shape.getType().equals("RECTANGLE")) {
-      Shape s = new Rectangle(shape.getName(), shape.getPositionX(),
-              shape.getPositionY(), toX, toY,
-              shape.getColor().getR(), shape.getColor().getG(), shape.getColor().getB());
-      s.setAppears(timeStart);
-      s.setDisappears(timeEnd);
-      return s;
-    }
-    return null;
-  }
-  
-  
-  @Override
-  public Shape changeColor(Shape shape, Color toColor, int timeStart, int timeEnd) {
-    return null;
-  }
   
   
 }
