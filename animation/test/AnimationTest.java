bcdcb3b69b5bce3cc369d1227e52412482058c9b
Ellah Nzikoba <ellahnzikoba@gmail.com>
2021-04-05 20:27:35 -0400

    style changes

--- animation/test/AnimationTest.java
+++ animation/test/AnimationTest.java
@@ -14,7 +14,9 @@ import animation.Scale;
 import animation.Shape;
 import animation.Transformation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 public class AnimationTest {
   private Animation ani;
