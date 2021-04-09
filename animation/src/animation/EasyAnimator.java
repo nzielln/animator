c49b24cf512409ebf8ea1d2b82384128a05a0c3c
Ellah Nzikoba <ellahnzikoba@gmail.com>
2021-04-09 19:00:43 -0400

    view interface

--- animation/src/animation/EasyAnimator.java
+++ animation/src/animation/EasyAnimator.java
@@ -13,20 +13,20 @@ public class EasyAnimator {
   public static void main(String[] args) throws FileNotFoundException {
     TextView text = new TextView();
     GraphicView graphic = new GraphicView();
-    SVGView svg = new SVGView();
+    SVGView svg = new SVGView(); //
     Animation m = new AnimationImpl(); //Model
     
-    //-------
+    //---------------------------------------------------------------------------------------------
     //This is the basic idea of getting file
     String fileInput = "smalldemo.txt"; //from the CLI - should have a method for this??
     String filename = "src/animation/code/" + fileInput;
     File demo = new File(filename);
     FileReader f = new FileReader(demo);
-    //-------
+    //---------------------------------------------------------------------------------------------
     
     AnimationBuilder<Animation> b = new AnimationBuilderImpl(m);
     AnimationReader.parseFile(f, b);
-    System.out.println(m.toString());
+    //System.out.println(m.toString());
   
   }
 }
