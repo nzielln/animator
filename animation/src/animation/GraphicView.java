c9e77f9afc88133a1e7a3d7d6e79dd5889f916c1
jianwhite <jianmwhite@gmail.com>
2021-04-10 14:55:59 -0400

    accidentally deleted work, pushing just for basics

--- animation/src/animation/GraphicView.java
+++ animation/src/animation/GraphicView.java
@@ -1,4 +1,22 @@
 package animation;
 
-public class GraphicView {
+import java.util.HashMap;
+
+import javax.swing.*;
+
+public class GraphicView extends JFrame implements View {
+  @Override
+  public HashMap<String, String> readInputs(Readable readable) {
+    return null;
+  }
+
+  @Override
+  public void readBuild(HashMap<String, String> inputs, Animation model) {
+
+  }
+
+  @Override
+  public void animate(Animation model, HashMap<String, String> inputs) {
+
+  }
 }
