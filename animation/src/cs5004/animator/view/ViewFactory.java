01a02d8f7c5be1fd27997c52d885148b8bc23de4
jianwhite <jianmwhite@gmail.com>
2021-04-16 16:37:18 -0400

    null checks view factory

--- animation/src/cs5004/animator/view/ViewFactory.java
+++ animation/src/cs5004/animator/view/ViewFactory.java
@@ -2,6 +2,7 @@ package cs5004.animator.view;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import cs5004.animator.model.Animation;
 import cs5004.animator.model.Shape;
@@ -9,7 +10,16 @@ import cs5004.animator.util.AnimationReader;
 
 public class ViewFactory {
   
-  public View create(HashMap<String, String> in, List<Shape> shapes, int width, int height, int x, int y) {
+  public View create(HashMap<String, String> in, List<Shape> shapes,
+                     int width, int height, int x, int y) {
+
+    Objects.requireNonNull(in, "Inputs can't be null");
+    Objects.requireNonNull(shapes, "Shapes list can't be null");
+
+    if (width < 0 || height < 0 || x < 0 || y < 0) {
+      throw new IllegalArgumentException("Width, height, x, and y must be positive");
+    }
+
     if (in.get("view").equals("text")) {
       return new TextView();
     } else if (in.get("view").equals("svg")) {
